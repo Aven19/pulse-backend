eb93a8f34201dd001991a00e67675af04f0787bc
Aven Mathias <51774683+Aven19@users.noreply.github.com>
2024-01-04 16:36:49 +0530

    Create __init__.py

---
+++ app/models/__init__.py
@@ -0,0 +1,3 @@
+from app.models.account import Account # type: ignore  # noqa: FKA100
+from app.models.user import User # type: ignore  # noqa: FKA100
+from app.models.user_account import UserAccount # type: ignore  # noqa: FKA100
